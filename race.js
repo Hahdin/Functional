630abdd6df527c26c8974d4056d2b3981fe75a61
Mark Foster <mark.foster@abracontrols.com>
2019-04-27 16:46:19 -0600

    update, remove impure use of console log

--- race.js
+++ race.js
@@ -4,18 +4,18 @@
  * Randomly move a car
  * @param {array} carPositions 
  */
-const movePositions = (carPositions) =>{
-  return carPositions.map((x,i) => Math.random() > 0.3 ? x+1 : x )
+const movePositions = (carPositions) => {
+  return carPositions.map((x, i) => Math.random() > 0.3 ? x + 1 : x)
 }
 
 /**
  * Output the current position
  * @param {number} carPosition 
  */
-const outputCarPosition = (carPosition) =>{
+const outputCarPosition = (carPosition) => {
   let count = carPosition
   let rt = ''
-  while(count--){
+  while (count--) {
     rt += '-'
   }
   return rt;
@@ -25,15 +25,16 @@ const outputCarPosition = (carPosition) =>{
  * Run a step of the race
  * @param {object} state 
  */
-const runStepOfRace = (state) =>{
+const runStepOfRace = (state) => {
   let hist = state['history'] !== undefined ? [...state['history']] : []
   return {
-    time: state.time -1,
+    ...state,
+    time: state.time - 1,
     carPositions: movePositions(state.carPositions),
     history: hist.concat({//keep a history
-      time:state.time, 
-      carPositions:[...state.carPositions],
-    })
+      time: state.time,
+      carPositions: [...state.carPositions],
+    }),
   }
 }
 
@@ -41,40 +42,62 @@ const runStepOfRace = (state) =>{
  * Draw the current state of the race
  * @param {object} state 
  */
-const drawState = (state) =>{
-  console.log(`
-    ${state.carPositions.map(outputCarPosition)}
-  `)
+const drawState = (state) => {
+  state.msgs.push(state.carPositions.map(outputCarPosition))
 }
 
 /**
  * Start the race
  * @param {object} state 
  */
-const race = (state) =>{
+const race = (state) => {
   drawState(state)
-  if (state.time){
+  if (state.time) {
     race(runStepOfRace(state))
   }
-  else{
-    console.log(`
+  else {
+    state.msgs.push(
+      `
     Win
     ======================
     `)
-    console.table(`
+    state.msgs.push(`
     time: ${state.time}
     car Positions: ${state.carPositions}
     `)
-    console.log(`
+    state.msgs.push(`
     History
     ======================
     `)
-    console.table(state.history)
+    state.msgs.push(state.history)
   }
 }
 
+/**
+ * Print the race results (impure)
+ * @param {object} state 
+ */
+const printResults = (state) => {
+  let table = false
+  state.msgs.forEach(msg => {
+    let txt = msg
+    if (Array.isArray(msg)) {
+      txt = ''
+      msg.forEach(i => txt += `${i},`)
+    }
+    table ? console.table(msg) : console.log(txt);
+    if (!Array.isArray(msg) && msg.search(/history/i) >= 0) {
+      table = true;
+    }
+  })
+}
+
 //Run a race
-race({
-  time: 15,
-  carPositions: [1,1,1],
-})
+let raceState = {
+  time: 5,
+  carPositions: [1, 1, 1],
+  msgs: [],
+}
+
+race(raceState)
+printResults(raceState)
